e5d9f5c5fe91ccd39ddd36e5e23f279f816eeeff
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-03-20 09:22:30 -0300

    adicionando migration, model e controller da Tarefa

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Request;
+
 abstract class Controller
 {
-    //
+    /** */
+    
+    
+    public function index(){
+
+    }
+    
+    
+    public function store (Request $request){
+
+    }
+
+    public function show(string $id){
+        
+    }
+
+
+    public function update(Request $request, string $id ){
+
+    }
+
+    public function destroy(string $id){
+
+    }
 }
